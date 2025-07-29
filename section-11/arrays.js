24e5e07290a4d4656756af98ea4658d859e442b8
Kikson9 <tochukwuokike7@gmail.com>
2025-07-29 23:22:29 +0800

    added more array methods

--- section-11/arrays.js
+++ section-11/arrays.js
@@ -78,3 +78,67 @@ const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'USD', 'EUR']);
 currenciesUnique.forEach(function (value, _, set) {
   console.log(`${value}: ${value}`);
 });
+
+///////////////////////////////////////
+// Coding Challenge #1
+/*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 
+*/
+
+const Julia = [3, 5, 2, 12, 7];
+const Kate = [4, 1, 15, 8, 3];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const newDogsJulia = dogsJulia.slice(1, -2);
+  const dogsTotal = [...newDogsJulia, ...dogsKate];
+  console.log(dogsTotal);
+  console.log(dogsJulia);
+  dogsTotal.forEach(function (value, i, _) {
+    if (value >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${value} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+
+checkDogs(Julia, Kate);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// MAP array mathod
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i) => {
+  return `Movement ${i + 1}: You ${
+    mov > 0 ? 'deposited' : 'withdrew'
+  } ${Math.abs(mov)}`;
+});
+
+console.log(movementsDescriptions);
