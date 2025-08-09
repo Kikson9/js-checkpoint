2d13a37b82fb46f2a578fd5c5a337af1eedfd01f
Kikson9 <tochukwuokike7@gmail.com>
2025-08-09 23:46:15 +0800

    working with a tabbed component

--- section-13/advanced-dom.js
+++ section-13/advanced-dom.js
@@ -219,3 +219,35 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+
+// Working with a Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabs.forEach(t =>
+  t.addEventListener('click', () => {
+    console.log('TAB');
+  })
+);
+// Above is Bad practice and the below showuld be followed instead
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
