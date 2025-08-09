c111957b55b151c82b504d685dc44b955875e87b
Kikson9 <tochukwuokike7@gmail.com>
2025-08-09 19:02:55 +0800

    added page navigation

--- section-13/advanced-dom.js
+++ section-13/advanced-dom.js
@@ -84,7 +84,7 @@ logo.classList.remove('c', 'j');
 logo.classList.toggle('c');
 logo.classList.contains('c'); // not includes
 
-/// Adding scroll properties 
+/// Adding scroll properties
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
@@ -96,9 +96,13 @@ btnScrollTo.addEventListener('click', function (e) {
   console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset); // depreceiated
   console.log('Current scroll (X/Y)', window.scrollX, scrollY);
 
-  console.log('height/width viewport',document.documentElement.clientHeight, document.documentElement.clientWidth);
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
 
-  Scrolling
+  Scrolling;
   window.scrollTo(
     s1coords.left + window.pageXOffset,
     s1coords.top + window.pageYOffset
@@ -124,12 +128,63 @@ h1.addEventListener('mouseenter', alertH1);
 
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
-
-h1.onmouseenter = function (e) {    // old way of adding event listeners
+h1.onmouseenter = function (e) {
+  // old way of adding event listeners
   alert('addEventListener: Great! You are reading the heading :D');
 };
 
+/////////////////////////
+// Event Propagation: Event Bubbling and Capturing in Practice
+// rgb(255, 255, 255)
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.target === e.currentTarget);
 
+  // Stop propagation
+  e.stopPropagation();
+});
 
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+
+//////////////////////////////////////
+// Page navigation
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+//  INSTEAD OF THE ABOVE WAY RATHER USE EVENT DELEGATION
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+  e.preventDefault();
+
+  // Matching strategy: to ignnore clicks that did not happen on the links we need
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
