4475f6f70eb5bdd0a817c4d73232e5107f80dc8b
Kikson9 <tochukwuokike7@gmail.com>
2025-08-13 01:25:23 +0800

    lifecycle dom events

--- section-13/advanced-dom.js
+++ section-13/advanced-dom.js
@@ -379,3 +379,18 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imageTarget.forEach(img => imgObserver.observe(img));
+
+// Lifecycle DOM events
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log(`Page fully loaded`, e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  // e.returnValue = 'message';
+});
