b6e168a85bf01975576ff6466266bca82190c001
Kikson9 <tochukwuokike7@gmail.com>
2025-08-19 04:26:01 +0800

    project architecture refactoring

--- section-15/working-with-OOP.js
+++ section-15/working-with-OOP.js
@@ -4,7 +4,7 @@ let map, mapEvent;
 
 // Working with the geolocation API and leaflet library
 if (navigator.geolocation) {
-  // for if the browsr supports the API
+  // for if the browser supports the API
   navigator.geolocation.getCurrentPosition(
     function (position) {
       // first function for if it successfully reads the location
@@ -60,6 +60,50 @@ form.addEventListener('submit', function (e) {
 
 //  NOW USING CLASSES AND OBJECTS WE REFACTOR TO:
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Running([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+///////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
