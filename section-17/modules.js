ed92257ebe4517484ffc1ce8ee7c360915833529
Kikson9 <tochukwuokike7@gmail.com>
2025-08-27 03:44:24 +0800

    the module pattern and commonJS modules

--- section-17/modules.js
+++ section-17/modules.js
@@ -34,3 +34,44 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+////////////////////////////////////
+// The module pattern
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product}  added to cart`);
+  };
+
+  const addToStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+
+//////////////////////////////////
+// CommmonJS Modules
+// Export
+// will not work here but just for reference as it is used in Node.js
+// export.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product}  added to cart`);
+// };
+
+// Import
+const { addToCart } = require('./shoppingCart.js');
