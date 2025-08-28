1a70f77f42479c510340151c6712989c383eb4f0
Kikson9 <tochukwuokike7@gmail.com>
2025-08-29 05:13:26 +0800

    configuring babel and polyfilling

--- section-17/modules.js
+++ section-17/modules.js
@@ -99,3 +99,27 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+/////////////////////////////////////
+// Configuring Babel and Polyfilling
+class Person {
+  #greeting = 'Hey';
+
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Jonas');
+console.log('jonas' ?? null);
+
+// console.log(state.cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(res => console.log(res));
+
+import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// For polyfilling async functions
+import 'regenerator-runtime/runtime';
